d91158acc8998f3a1c90d77d11de88d297ee4f05
Randall Smith <rdsmith1761@gmail.com>
2023-01-06 14:54:25 -0500

    Exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
@@ -9,12 +11,31 @@
             int quotient = a / b;
             int remainder = a % b;
 
-            if(a==17 && b == 4)
+            if (a == 17 && b == 4)
             {
                 Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             }
+            else
+            {
+                return;
+            }
 
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            AreaOfCircle(radius);
+            Console.WriteLine(AreaOfCircle(radius));
 
         }
+            public static double AreaOfCircle(double radius)
+            {
+                
+                var area = Math.PI * Math.Pow(radius, 2);
+                return area;
+            }
+        
+
     }
 }
